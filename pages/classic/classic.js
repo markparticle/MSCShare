700386ccb87f6ad88838dbc3d9539788d3f37b49
unknown <unknown@example.com>
2019-05-27 20:44:22 +0800

    [ADD]like组件、封装HTTP

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,19 +1,39 @@
 // pages/classic/classic.js
+import {HTTP} from '../../util/http.js'
+
+
+let http = new HTTP()
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    test:1
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
-  },
+    http.request({
+      url: 'classic/latest',
+      success: (res) => {
+        console.log(res)
+      }
+    })
+    },
+  //   wx.request({
+  //     url: 'http://bl.7yue.pro/v1/classic/latest',
+  //     header: {
+  //       appkey: "RdshydjBvcYZhMZC"
+  //     },
+  //     success:(res) => {
+  //       console.log(this.data.test)
+  //     },
+  //   })
+  // },
 
   /**
    * Lifecycle function--Called when page is initially rendered
