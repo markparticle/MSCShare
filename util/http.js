d3a433a1e1b94b3205802d3516dde7d9b6ae6977
unknown <unknown@example.com>
2019-05-27 21:03:43 +0800

    [ADD]完善HTTP异常处理

--- util/http.js
+++ util/http.js
@@ -1,4 +1,12 @@
 import {config} from '../config.js'
+
+
+const tips = {
+    1: '抱歉，出现了一个错误',
+    1005: 'appkey无效',
+    3000: '期刊不存在'
+}
+
 class HTTP{
     request(params){
         // URL, data, method
@@ -16,15 +24,29 @@ class HTTP{
             success:(res)=>{
                 let code = res.statusCode.toString()
                 if(code.startsWith('2')){
-
+                    params.success(res.data)
                 }
-                else{
-
+                else{ 
+                    //服务器异常
+                    let error_code = res.data.error_code
+                    this._show_error(error_code)
                 }
             },
             fail:(err)=>{     
-                       
+                //调用失败 
+                this._show_error(1)
             }
+        })
+    }
+
+    _show_error(error_code){
+        if(!error_code){
+            error_code = 1
+        }
+        wx.showToast({
+            title:tips[error_code],
+            icon: 'none',
+            duration: 2000
         })
     }
 }
