a084664c6bcf40a877596af5f1a4df84d8b06dc0
MARK <unknown@example.com>
2019-07-09 21:56:43 +0800

    [ADD]短评功能

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -2,9 +2,13 @@
 import {
     BookModel
 } from '../../models/book.js'
+import {
+    LikeModel
+} from '../../models/like.js';
 
 
-let bookModel = new BookModel()
+const bookModel = new BookModel()
+const likeModel = new LikeModel()
 
 Page({
 
@@ -16,6 +20,7 @@ Page({
         book: null,
         likeStatus: false,
         likeCount: 0,
+        posting: false,
     },
 
     /**
@@ -45,6 +50,51 @@ Page({
                 likeCount: res.fav_nums,
             })
         })
+    },
+
+    onLike(event) {
+        const like_or_cancel = event.detail.behavior
+        likeModel.like(like_or_cancel, this.data.book.id, 400)
+    },
+
+    onFakePost(event) {
+        this.setData({
+            posting: true
+        })
+    },
+
+    onCancel(event) {
+        this.setData({
+            posting: false
+        })
+    },
+
+    onPost(event) {
+        const comment = event.detail.text
+        console.log(event.detail.text)
+        // if (comment.length > 12) {
+        //     wx.showToast({
+        //         title: '短评最多12个字',
+        //         icon: 'none'
+        //     })
+        //     return
+        // }
+        bookModel.postComment(this.data.book.id, comment)
+            .then(res => {
+                wx.showToast({
+                    title: '+1',
+                    icon: 'none',
+                })
+
+                this.data.comments.unshift({
+                    comment,
+                    nums: 1,
+                })
+
+                this.setData({
+                    comments: this.data.comments
+                })
+            })
     },
 
     /**
